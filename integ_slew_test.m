d4660341b9dc74ddf4f3f10257f4a4140bfddd6d
seijirom <seijiro.moriyama@gmail.com>
2016-08-20 13:12:16 +0900

    slew.m and integ_slew updated

--- integ_slew_test.m
+++ integ_slew_test.m
@@ -9,17 +9,17 @@ delta = Cin/(Co+(Co+Cin)/A);
 gamma = 1/(1+Cin/((A+1)*Co));
 VinHi = 2; SR = 10; GBW = 1; 
 sim('integ_gain_slew_test') % simulation???s
-%figure(1)
-%stairs(1:N,Vin(1:N),'b'); hold on; stairs(1:N,Vout(1:N),'r');axis([0 N 0 N]);
+figure(1)
+stairs(1:N,Vin(1:N),'b'); hold on; stairs(1:N,Vout(1:N),'r');axis([0 N 0 N]);
 
-% windowBM=blackman(N);
-% %voutfft=fft(vout)/(N/2);  % window ????
-% voutfft=fft(vout.*windowBM)/(N/2);
-% voutfftdB=20*log10(abs(voutfft));
-% xx=linspace(0,1,N);
-% figure(2)
-% plot(xx,voutfftdB,'b-');
-% figure(3)
-% semilogx(xx,voutfftdB,'b-') % semilog plot
-% axis([0.001 0.5 -100 0]);
-% 
+ windowBM=blackman(N);
+ %voutfft=fft(vout)/(N/2);  % window ????
+ voutfft=fft(vout.*windowBM)/(N/2);
+ voutfftdB=20*log10(abs(voutfft));
+ xx=linspace(0,1,N);
+ figure(2)
+ plot(xx,voutfftdB,'b-');
+ figure(3)
+ semilogx(xx,voutfftdB,'b-') % semilog plot
+ axis([0.001 0.5 -100 0]);
+ 
